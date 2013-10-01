061739fab9c67a336e74c9bf0b6a9a09f9d1a0d3
Christophe Ladroue <christophe@downhouse>
2013-10-01 12:44:44 +0100

    actually adding the files.

---
+++ roadblock.xtext.xpect.ibl/src/roadblock/xtext/xpect/ibl/generator/IBLGeneratorTest.java
@@ -0,0 +1,12 @@
+package roadblock.xtext.xpect.ibl.generator;
+
+import org.junit.runner.RunWith;
+import org.xpect.runner.XpectRunner;
+import org.xpect.runner.XpectTestFiles;
+import org.xpect.xtext.lib.tests.GeneratorTest;
+
+@RunWith(XpectRunner.class)
+@XpectTestFiles(fileExtensions = "xt")
+public class IBLGeneratorTest extends GeneratorTest {
+
+}
---
+++ roadblock.xtext.xpect.ibl/src/roadblock/xtext/xpect/ibl/validation/IBLValidationTest.java
@@ -0,0 +1,12 @@
+package roadblock.xtext.xpect.ibl.validation;
+
+import org.junit.runner.RunWith;
+import org.xpect.runner.XpectRunner;
+import org.xpect.runner.XpectTestFiles;
+import org.xpect.xtext.lib.tests.ValidationTest;
+
+@RunWith(XpectRunner.class)
+@XpectTestFiles(fileExtensions = "xt")
+public class IBLValidationTest extends ValidationTest {
+
+}
