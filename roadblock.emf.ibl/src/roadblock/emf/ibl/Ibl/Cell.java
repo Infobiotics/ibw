45d52e9547fdbf52eb6a2bcb8fbe07a865324de8
Christophe Ladroue <christophe@downhouse>
2013-10-03 15:47:30 +0100

    Added one class for all tests. Added javadoc.

--- roadblock.emf.ibl/src/roadblock/emf/ibl/Ibl/Cell.java
+++ roadblock.emf.ibl/src/roadblock/emf/ibl/Ibl/Cell.java
@@ -11,6 +11,10 @@ import org.eclipse.emf.ecore.EObject;
  * A representation of the model object '<em><b>Cell</b></em>'.
  * <!-- end-user-doc -->
  *
+ * <!-- begin-model-doc -->
+ * Attempt at generating javadoc from EMF.
+ * <!-- end-model-doc -->
+ *
  * <p>
  * The following features are supported:
  * <ul>
