c734d143e313978f8a905363aebc8beba320cd1d
laurentiu <Laurentiu.Mierla@gmail.com>
2014-01-19 12:57:42 +0200

    Set up the initial structure for populating the data model from the semantic model.

---
+++ roadblock.datapopulation/src/roadblock/datapopulation/ModelBuilder.java
@@ -0,0 +1,8 @@
+package roadblock.datapopulation;
+
+import roadblock.emf.ibl.Ibl.Model;
+import roadblock.xtext.ibl.ibl.util.IblSwitch;
+
+public class ModelBuilder extends IblSwitch<Model>{
+
+}
---
+++ roadblock.datapopulation/src/roadblock/datapopulation/PropertyBuilder.java
@@ -0,0 +1,15 @@
+package roadblock.datapopulation;
+
+import roadblock.emf.ibl.Ibl.IProperty;
+import roadblock.xtext.ibl.ibl.ProbabilityProperty;
+import roadblock.xtext.ibl.ibl.util.IblSwitch;
+
+public class PropertyBuilder extends IblSwitch<IProperty>{
+
+	@Override
+	public IProperty caseProbabilityProperty(ProbabilityProperty object) {
+		// TODO Auto-generated method stub
+		return super.caseProbabilityProperty(object);
+	}
+	
+}
