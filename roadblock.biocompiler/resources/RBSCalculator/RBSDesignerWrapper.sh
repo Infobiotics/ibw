62834041c6e776eabfd4d01e0ce504a1a186b92d
Christophe <christophe@dell>
2015-01-12 17:06:51 +0000

    biocompiler: script for building binaries for linux and Mac.

--- roadblock.biocompiler/resources/RBSCalculator/RBSDesignerWrapper.sh
+++ roadblock.biocompiler/resources/RBSCalculator/RBSDesignerWrapper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # wrapper for RBS_Design
+#./RBSDesignerWrapper.sh CTAGGTACAGTGCTAGCTtctaga atggtgaatgtgaaaccagtaacgttatacgatgt 1000.25 
 # C.Ladroue
 
 cd `dirname $0` 
@@ -10,7 +11,6 @@ export NUPACKHOME="$(pwd)/nupack3.0.4"
 
 
 # run RBS_Design
-#./Run_RBS_Design.py CTAGGTACAGTGCTAGCTtctaga atggtgaatgtgaaaccagtaacgttatacgatgt 1000.25 
 ./Run_RBS_Design.py $1 $2 $3
 
 
