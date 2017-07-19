bc81dd11ff31b71f69bad38699ba21e2c32089e8
Konur <Konur@neweduroam-10-61-3-50.ncl.ac.uk>
2017-07-19 19:09:53 +0100

    MacOS ngssWrapper addition

---
+++ roadblock.bin.mac_x86_64/resources/ngss/ngssWrapper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# clear the simulation output directory
+rm -rf $1 && mkdir -p $1 &&
+
+# run ngss simulator
+cd $1 && 
+exec "`dirname $0`/ngss" --emf parser=emf max_time=$2 max_runtime=$3 simulation_algorithm=$4 data_file=$5 log_interval=$6 runs=$7 seed=$8 output=csv compress=true parallel=true show_progress=false
