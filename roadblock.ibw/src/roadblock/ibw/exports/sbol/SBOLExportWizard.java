b633501f54be3f53ebc7532d2abf57ef479ab6ad
Laurentiu <laurentiu.mierla@gmail.com>
2017-07-26 17:21:41 +0300

    Added more complex SBOL export UI functionality

--- roadblock.ibw/src/roadblock/ibw/exports/sbol/SBOLExportWizard.java
+++ roadblock.ibw/src/roadblock/ibw/exports/sbol/SBOLExportWizard.java
@@ -1,12 +1,21 @@
 package roadblock.ibw.exports.sbol;
 
+import java.util.List;
+
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IExportWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.eclipse.ui.internal.wizards.datatransfer.DataTransferMessages;
+import org.eclipse.ui.wizards.datatransfer.ZipFileExportWizard;
 
+@SuppressWarnings("restriction")
 public class SBOLExportWizard extends Wizard implements IExportWizard {
 
+	private IStructuredSelection selection;
 	private SBOLExportWizardPage wizardPage;
 
 	@Override
@@ -16,25 +25,26 @@ public class SBOLExportWizard extends Wizard implements IExportWizard {
 
 	@Override
 	public void addPages() {
-		wizardPage = new SBOLExportWizardPage();
+		wizardPage = new SBOLExportWizardPage(selection);
 		addPage(wizardPage);
+		new ZipFileExportWizard();
 	}
 
 	@Override
 	public boolean performFinish() {
-
-		// Call here the code to perform the SBOL export
-		
-		return true;
+		return wizardPage.finish();
 	}
 
 	@Override
-	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	public SBOLExportWizard() {
+	public void init(IWorkbench workbench, IStructuredSelection currentSelection) {
+		this.selection = currentSelection;
+
+		List<?> selectedResources = IDE.computeSelectedResources(currentSelection);
+		if (!selectedResources.isEmpty()) {
+			this.selection = new StructuredSelection(selectedResources);
+		}
+
+		setWindowTitle(DataTransferMessages.DataTransfer_export);
 		setNeedsProgressMonitor(true);
 	}
 }
--- roadblock.ibw/src/roadblock/ibw/exports/sbol/SBOLExportWizardPage.java
+++ roadblock.ibw/src/roadblock/ibw/exports/sbol/SBOLExportWizardPage.java
@@ -1,57 +1,86 @@
 package roadblock.ibw.exports.sbol;
 
-import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.FileDialog;
-import org.eclipse.swt.widgets.Text;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SBOLExportWizardPage extends WizardPage {
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.internal.wizards.datatransfer.WizardFileSystemResourceExportPage1;
+
+@SuppressWarnings("restriction")
+public class SBOLExportWizardPage extends WizardFileSystemResourceExportPage1 {
 
 	private String exportFilePath;
-
-	private Composite container;
-
-	@Override
-	public void createControl(Composite parent) {
-		container = new Composite(parent, SWT.NONE);
-		GridLayout layout = new GridLayout();
-		container.setLayout(layout);
-		layout.numColumns = 2;
-
-		Button fileDialogButton = new Button(container, SWT.PUSH);
-		fileDialogButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 1, 1));
-		fileDialogButton.setText("Select path...");
-		final Text exportFileWidget = new Text(container, SWT.BORDER);
-		exportFileWidget.setEnabled(false);
-		exportFileWidget.setText("");
-		exportFileWidget.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 1, 1));
-		final FileDialog directoryDialog = new FileDialog(container.getShell());
-		fileDialogButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				exportFilePath = directoryDialog.open();
-				exportFileWidget.setText(exportFilePath);
-				setPageComplete(true);
-			}
-		});
-
-		// required to avoid an error in the system
-		setControl(container);
-		setPageComplete(false);
-	}
 
 	public String getExportFilePath() {
 		return exportFilePath;
 	}
 
-	public SBOLExportWizardPage() {
-		super("SBOL Export Settings");
+	@Override
+	@SuppressWarnings("unchecked")
+	public boolean finish() {
+		List<org.eclipse.core.internal.resources.File> resourcesToExport = (List<org.eclipse.core.internal.resources.File>) getWhiteCheckedResources();
+
+		return executeExportOperation(resourcesToExport, getDestinationValues(resourcesToExport));
+	}
+
+	protected boolean executeExportOperation(List<org.eclipse.core.internal.resources.File> resourcesToExport,
+			List<String> destinationValues) {
+		// Call here the code to perform the SBOL export
+
+		return false;
+	}
+
+	/**
+	 * Answer the contents of self's destination specification widget. If this value
+	 * does not have a suffix then add it first.
+	 */
+	protected List<String> getDestinationValues(List<org.eclipse.core.internal.resources.File> resourcesToExport) {
+		String idealSuffix = getOutputSuffix();
+
+		List<String> destinationValues = new ArrayList<String>();
+
+		// only append a suffix if the destination doesn't already have a . in
+		// its last path segment.
+		// Also prevent the user from selecting a directory. Allowing this will
+		// create a ".sbol" file in the directory
+		for (org.eclipse.core.internal.resources.File resourceToExport : resourcesToExport) {
+			String filePath = super.getDestinationValue();
+			String fileNameWithoutExtension = resourceToExport.getName().substring(0,
+					resourceToExport.getName().lastIndexOf('.'));
+
+			if (filePath.length() != 0 && !filePath.endsWith(File.separator)) {
+				int dotIndex = filePath.lastIndexOf('.');
+				if (dotIndex != -1) {
+					// the last path seperator index
+					int pathSepIndex = filePath.lastIndexOf(File.separator);
+					if (pathSepIndex != -1 && dotIndex < pathSepIndex) {
+						filePath += File.separator + fileNameWithoutExtension + idealSuffix;
+					}
+				} else {
+					filePath += File.separator + fileNameWithoutExtension + idealSuffix;
+				}
+			}
+
+			destinationValues.add(filePath);
+		}
+
+		return destinationValues;
+	}
+
+	/**
+	 * Answer the suffix that files exported from this wizard should have. If this
+	 * suffix is a file extension (which is typically the case) then it must include
+	 * the leading period character.
+	 *
+	 */
+	protected String getOutputSuffix() {
+		return ".sbol";
+	}
+
+	@SuppressWarnings("restriction")
+	public SBOLExportWizardPage(IStructuredSelection selection) {
+		super(selection);
 		setTitle("SBOL Export Settings");
 		setDescription("Please configure the details of the SBOL export");
 	}
