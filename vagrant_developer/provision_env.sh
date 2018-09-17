406c41622dae0d398c7fa0613d54130e30c6e750
peter <bodun@stanford.edu>
2018-09-17 16:59:51 +0100

    changed openjdk to oracle jdk

--- vagrant_developer/provision_env.sh
+++ vagrant_developer/provision_env.sh
@@ -6,7 +6,10 @@ PASSWORD=$2
 
 sudo apt-get update
 sudo apt-get -y install git
-sudo apt-get -y install openjdk-8-jdk
+sudo apt-get install -y python-software-properties debconf-utils
+sudo add-apt-repository -y ppa:webupd8team/java
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
 
 git clone https://$USERNAME:$PASSWORD@code.ico2s.org/roadblock.xtext
 cd roadblock.xtext
