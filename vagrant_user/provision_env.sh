36180c839eef78af6162bde5b2e179a38c32e0a4
Laurentiu Mierla <laurentiu.mierla@gmail.com>
2020-10-23 18:46:19 +0300

    Revert "Updated the vagrant configuration files in order to support Ubuntu 18.04."
    This reverts commit 921c463a7f80c734db77f18aa36c61af3bb030ed.

--- vagrant_user/provision_env.sh
+++ vagrant_user/provision_env.sh
@@ -2,17 +2,20 @@
 set -e
 
 # Headless installation of Oracle's Java 8
-sudo add-apt-repository -y ppa:ts.sch.gr/ppa
-sudo apt-get update
+sudo apt-get install -y python-software-properties debconf-utils software-properties-common
+export http_proxy="http://username:password@proxy:port/"
+export https_proxy="https://username:password@proxy:port/"
+sudo add-apt-repository -y ppa:webupd8team/java
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer
+sudo apt-get update
+sudo apt-get install -y --allow-unauthenticated oracle-java8-installer
+unset http_proxy
+unset https_proxy
 
 # Download IBW executable + supporting files
-wget --progress=bar:force infobiotics.org/_static/infobiotics.tar.gz
+wget --progress=bar:force infobiotics.org/files/infobiotics.tar.gz
 tar -xzf infobiotics.tar.gz
 rm infobiotics.tar.gz
 
 # Change ownership of directory from root to vagrant
 chown -R vagrant: infobiotics
-chmod +x ~/Desktop/ibw.desktop
-
